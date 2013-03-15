6d5471a75b44ef542ff182b47d1c6cf61bd41d8a
Jason Dulay <jasonkd@gmail.com>
2013-03-14 21:50:09 -0400

    first commit

---
+++ app/controllers/aaa_core_controller.rb
@@ -0,0 +1,17 @@
+class AaaCoreController < ApplicationController
+  def index
+  	@title="Home"
+  end
+
+  def events
+  	@title="Events"
+  end
+
+  def shop
+  	@title="Shop"
+  end
+
+  def discussion
+  	@title="Discussion Forum"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AaaWebsite::Application.config.secret_token = 'c0b72941a81b40eadf9cd711ba1d49547559fff06ce5e41a70255cb8dc1a03d434ae48ef546bd21396babd1c820c2e53a6b618f1801f6f8e444197d802a9ff34'
